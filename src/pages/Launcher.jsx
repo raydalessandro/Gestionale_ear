378d1e925054ed6a61a789b49e4a3936be224cc2
raydalessandro <alessio.marrone1993@gmail.com>
2025-11-16 17:07:27 +0100

    Update Launcher.jsx

--- src/pages/Launcher.jsx
+++ src/pages/Launcher.jsx
@@ -522,7 +522,7 @@ const calculateStats = (moduleId) => {
     </div>
   </div>
 ))}
-</div>
+
         
         {/* 📝 RECENT ACTIVITY */}
         <div style={{ display: 'grid', gridTemplateColumns: '2fr 1fr', gap: '30px', marginBottom: '50px' }}>
